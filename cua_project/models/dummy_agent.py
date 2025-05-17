25341ba1fbadfe90db2a240d42a2b62d6a373ff5
Matt Sárdi <sardi.matt@gmail.com>
2025-05-17 06:12:13 +0200

    fix(model): update DummyAgent

--- cua_project/models/dummy_agent.py
+++ cua_project/models/dummy_agent.py
@@ -5,10 +5,14 @@ from cua_project.models.base_agent import Action, BaseAgent, Observation
 
 
 class DummyAgent(BaseAgent):
+    def __init__(self, *args, **kwargs):
+        pass
+
     action_space = "pyautogui"
 
     def reset(self, logger: Optional[Logger] = None) -> None:
         pass
 
     def predict(self, instruction: str, obs: Observation) -> tuple[str, list[Action]]:
+        # return "", ["pyautogui.click(960, 540)"]
         return "", ["DONE"]
